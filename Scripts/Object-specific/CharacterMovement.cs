2dd01cb002b097d4a7a78fa15890472d0b38ca52
Rus <definitelyrus@gmail.com>
2024-12-31 22:28:01 +0800

    Added player-object collision + minor changes
    + refactors, deletions, and engine file updates.

--- Scripts/Object-specific/CharacterMovement.cs
+++ Scripts/Object-specific/CharacterMovement.cs
@@ -1,4 +1,5 @@
 using Godot;
+using System;
 
 public partial class CharacterMovement : CharacterBody3D
 {
@@ -20,25 +21,49 @@ public partial class CharacterMovement : CharacterBody3D
 	[Export] public bool ControlOverride = false;
 
 	/// <summary>
+	/// Allows the sprite layering to be overridden, making the character's sprite always appear at the top-most layer.
+	/// </summary>
+	[Export] public bool AlwaysOnTop = false;
+
+	[Export(PropertyHint.Range, "0,45,0.5")]
+	public float SpriteLayerDepth = 45f;
+
+	/// <summary>
+	/// This character's collider node.
+	/// Used to change the parent node's Z position relative to its Y position.
+	/// </summary>
+	[ExportGroup("Nodes")]
+	[Export] public CollisionShape3D collider;
+
+	/// <summary>
+	/// This character's sprite.
+	/// </summary>
+	[Export] public Sprite3D sprite;
+
+	/// <summary>
 	/// How quickly the character moves when walking.
 	/// </summary>
 	[ExportGroup("Physics")]
-	[Export] public float WalkingSpeed = 3.5f;
+	[Export(PropertyHint.Range, "0,100,0.1")]
+	public float WalkingSpeed = 3.5f;
 
 	/// <summary>
 	/// How quickly the character moves when running.
 	/// </summary>
-	[Export] public float RunningSpeed = 5.0f;
+	[Export(PropertyHint.Range, "0,100,0.1")]
+	public float RunningSpeed = 5.0f;
 
 	/// <summary>
 	/// How quickly the character will reach its target speed.
 	/// </summary>
-	[Export] public float Acceleration = 20f;
+	[Export(PropertyHint.Range, "0,100,0.1")]
+	public float Acceleration = 20f;
 
 	/// <summary>
 	/// How quickly the character will slow to a halt.
 	/// </summary>
-	[Export] public float Friction = 20f;
+	[Export(PropertyHint.Range, "0,100,0.1")]
+	public float Friction = 30f;
 
 	/// <summary>
 	/// A multiplier to the target speed.
@@ -49,10 +74,11 @@ public partial class CharacterMovement : CharacterBody3D
 	/// 1.0f = The character moves at its intended rate. <br/>
 	/// 1.1f = The character moves faster.
 	/// </summary>
-	[Export] public float StatusMultiplier = 1f;
+	[Export(PropertyHint.Range, "0, 10, 0.05")]
+	public float StatusMultiplier = 1f;
 
-	public override void _PhysicsProcess(double delta)
-	{
+	public override void _PhysicsProcess(double delta) {
+
 		//Character direction heading can be overridden.
 		if (ControlOverride) {
 			Direction = Vector3.Zero;
@@ -66,12 +92,22 @@ public partial class CharacterMovement : CharacterBody3D
 		if (Direction != Vector3.Zero) {
 			targetSpeed = WalkingSpeed; //TODO: Allow running speed.
 			Velocity = Velocity.MoveToward(Direction.Normalized() * targetSpeed * StatusMultiplier, Acceleration * (float) delta);
+
+			GD.Print(
+				$"CharacterBody YZ:    {GlobalPosition.Y:F2} {GlobalPosition.Z:F2}\n" +
+				$"Sprite3D YZ:         {sprite.GlobalPosition.Y:F2} {sprite.GlobalPosition.Z:F2}\n" +
+				$"CollisionShape3D YZ: {collider.GlobalPosition.Y:F2} {collider.GlobalPosition.Z:F2}"
+			);
 		}
+
 
 		//Otherwise, decelerate to a stop.
 		else Velocity = Velocity.MoveToward(Vector3.Zero, Friction * (float) delta);
 		
 		//Apply the Velocity value to the CharacterBody3D Node.
 		MoveAndSlide();
+
+		//Updates the character's Z position to negatively match the Y position, allowing for natural sprite layering.
+		if (Velocity != Vector3.Zero) sprite.GlobalPosition = new Vector3(sprite.GlobalPosition.X, sprite.GlobalPosition.Y, AlwaysOnTop ? SpriteLayerDepth : -collider.GlobalPosition.Y);
 	}
 }
